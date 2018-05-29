e7317644d677f8a3f0a8e41410e994e7debaaff5
Kaycheng <kaycheng717@gmail.com>
2018-05-29 23:57:29 +0800

    Create admin post edit, update and destroy function

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
     
   def index
     @posts = Post.all
@@ -15,6 +15,18 @@ class Admin::PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.save
     flash[:notice] = "Great!"
+    redirect_to admin_posts_path
+  end
+
+  def update
+    @post.update(post_params)
+    flash[:notice] = "文章已修改完成"
+    redirect_to admin_posts_path
+  end
+
+  def destroy
+    @post.delete
+    flash[:alert] = "文章已被刪除"
     redirect_to admin_posts_path
   end
 
