afdb66bf149702c2e2d89431514eb639654aa528
Kaycheng <kaycheng717@gmail.com>
2018-05-29 23:31:29 +0800

    Create admin posts index and new pages

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,6 +3,23 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin
     
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    flash[:notice] = "Great!"
+    redirect_to admin_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image)
   end
 end
