36913b2a9b606d066e77df1e1c7d4f646f7ac0d6
Kaycheng <kaycheng717@gmail.com>
2018-05-29 22:47:14 +0800

    Setup user role for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to posts_path
+    end
+  end
 end
